Fix masking

soc/models/resnet18bilstm_policy.py
@@ -227,8 +227,7 @@ class ResNet18BiLSTMConcatPolicy(nn.Module):
         output = torch.sum(
             output * x_text_mask.unsqueeze(-1).unsqueeze(-1), dim=(1, 2)
         )  # [bs, F_bert]
-        output = output / torch.sum(x_text_mask, dim=1, keepdim=True)
-        output[torch.isnan(output)] = 0.
+        output = output / (torch.sum(x_text_mask, dim=1, keepdim=True) + 1e-7)
 
         # Fusion
         z_linear = torch.cat([z_linear, output], dim=1)
@@ -461,8 +460,7 @@ class ResNet18BiLSTMFFPolicy(ResNet18BiLSTMConcatPolicy):
         output = torch.sum(
             output * x_text_mask.unsqueeze(-1).unsqueeze(-1), dim=(1, 2)
         )  # [bs, F_bert]
-        output = output / torch.sum(x_text_mask, dim=1, keepdim=True)
-        output[torch.isnan(output)] = 0.
+        output = output / (torch.sum(x_text_mask, dim=1, keepdim=True) + 1e-7)
 
         # Fusion
         z_linear = torch.cat([z_linear, output], dim=1)
@@ -503,8 +501,7 @@ class ResNet18BiLSTMFFResPolicy(ResNet18BiLSTMFFPolicy):
         output = torch.sum(
             output * x_text_mask.unsqueeze(-1).unsqueeze(-1), dim=(1, 2)
         )  # [bs, F_bert]
-        output = output / torch.sum(x_text_mask, dim=1, keepdim=True)
-        output[torch.isnan(output)] = 0.
+        output = output / (torch.sum(x_text_mask, dim=1, keepdim=True) + 1e-7)
 
         # Residual fusion
         z_linear_fus = torch.cat([z_linear, output], dim=1)

soc/models/resnet18mean_policy.py
@@ -214,8 +214,7 @@ class ResNet18MeanConcatPolicy(nn.Module):
         x_text = x_text[:, -1]  # [bs, S_text, F_bert]
         x_text_mask = x_text_mask[:, -1]  # [bs, S_text]
         x_text = torch.sum(x_text * x_text_mask.unsqueeze(-1), dim=1)  # [bs, F_bert]
-        x_text = x_text / torch.sum(x_text_mask, dim=1, keepdim=True)
-        x_text[torch.isnan(x_text)] = 0.
+        x_text = x_text / (torch.sum(x_text_mask, dim=1, keepdim=True) + 1e-7)
 
         # Fusion
         z_linear = torch.cat([z_linear, x_text], dim=1)
@@ -435,8 +434,7 @@ class ResNet18MeanFFPolicy(ResNet18MeanConcatPolicy):
         x_text = x_text[:, -1]  # [bs, S_text, F_bert]
         x_text_mask = x_text_mask[:, -1]  # [bs, S_text]
         x_text = torch.sum(x_text * x_text_mask.unsqueeze(-1), dim=1)  # [bs, F_bert]
-        x_text = x_text / torch.sum(x_text_mask, dim=1, keepdim=True)
-        x_text[torch.isnan(x_text)] = 0.
+        x_text = x_text / (torch.sum(x_text_mask, dim=1, keepdim=True) + 1e-7)
 
         # Fusion
         z_linear = torch.cat([z_linear, x_text], dim=1)
@@ -471,8 +469,7 @@ class ResNet18MeanFFResPolicy(ResNet18MeanFFPolicy):
         x_text = x_text[:, -1]  # [bs, S_text, F_bert]
         x_text_mask = x_text_mask[:, -1]  # [bs, S_text]
         x_text = torch.sum(x_text * x_text_mask.unsqueeze(-1), dim=1)  # [bs, F_bert]
-        x_text = x_text / torch.sum(x_text_mask, dim=1, keepdim=True)
-        x_text[torch.isnan(x_text)] = 0.
+        x_text = x_text / (torch.sum(x_text_mask, dim=1, keepdim=True) + 1e-7)
 
         # Residual fusion
         z_linear_fus = torch.cat([z_linear, x_text], dim=1)